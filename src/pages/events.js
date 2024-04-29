1f1baae519825191943dd3d1306d941a501fd341
Jason Geiger <448523+jasonjgeiger@users.noreply.github.com>
2024-04-28 18:17:03 -0700

    Event styles

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
+import styled from 'styled-components'
 import get from "lodash/get"
 
 import SEO from "../components/seo"
@@ -10,6 +11,27 @@ import Article from "../components/article"
 import Column from "../components/column"
 
 import heroImage from "../assets/images/329693658_6036260803100793_7836501218673709703_n.jpg"
+
+const PageStyling = styled.div`
+  h2,h3,h4,blockquote{
+    margin:0;
+    padding: var(--spacingVerticalM) 0 0;
+    text-align:left;
+  }
+  h2{
+    font-size: var(--fontSizeHero900);
+  }
+  h3{
+    font-size: var(--fontSizeHero700);
+  }
+  h3+p {
+    margin-top: var(--spacingVerticalXL);
+  }
+  hr{
+    margin: var(--spacingVerticalXXXL) 0 var(--spacingVerticalL);
+    width:100%;
+  }
+`;
 
 const EventPage = (props) => {
   const {title, description, siteUrl} = get(props, 'data.site.siteMetadata');
@@ -25,7 +47,9 @@ const EventPage = (props) => {
       />
       <Article>
         <Column flex="1 0 0">
-          {renderRichText(page.content)}
+          <PageStyling>
+            {renderRichText(page.content)}
+          </PageStyling>
         </Column>
       </Article>
     </Layout>
